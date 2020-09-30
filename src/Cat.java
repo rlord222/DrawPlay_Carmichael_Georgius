1736b9c48b3462f41b9ec7bca2da8c6aabaf98e0
carmichaeljr <carmichaeljr@outlook.com>
2020-09-29 19:06:57 -0600

    Changed text color

--- src/Cat.java
+++ src/Cat.java
@@ -76,7 +76,7 @@ public class Cat {
 		g2.setColor(Color.red);
 		g2.fillOval(FISH_EYE_X, FISH_EYE_Y, FISH_EYE, FISH_EYE);
 		
-		g2.setColor(Color.black);
+		g2.setColor(Color.blue);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("MEOW", catX, catY+HEAD_DIMENSION+10);	
